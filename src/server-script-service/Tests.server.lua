39ec67ecbd9981ed19d712c7078068453b0153bc
andrewens <andrewens4@gmail.com>
2023-10-06 08:47:18 -0500

    Run unit tests w/ TestEZ

--- src/server-script-service/Tests.server.lua
+++ src/server-script-service/Tests.server.lua
@@ -1,1 +1,12 @@
-print("Hello world, from server!")
+-- dependency
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TestsFolder = ReplicatedStorage:FindFirstChild("roblox-css-tests")
+
+local TestEZ = require(ReplicatedStorage:FindFirstChild("TestEZ"))
+
+--[[
+    Run unit tests for roblox-css
+    Even though this is a front-end library for clients, I run the tests
+    on the server because it's faster to hit `Run` than `Play` in ROBLXO STUDIO 
+]]
+TestEZ.TestBootstrap:run({ TestsFolder })
